a637e126164cf6ee06d3d3e47f13df27b7f0181a
Diego Borjas <diegoborjas@gmail.com>
2023-04-24 18:29:35 -0500

    added functionality to fetch current weather, and started to populate info into the 5 day weather forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,6 @@ function fetchCoordinates(city) {
     return fetch(GEO_CODING_API)
         .then(function (res) {
             if (!res.ok) throw new Error ('Error!');
-            // console.log(res)
             return res.json();
         })
         .then(function (data) {
@@ -23,11 +22,24 @@ function fetchWeatherForecast(lat, lon) {
     return fetch(FORECAST_API)
         .then(function (res) {
             if (!res.ok) throw new Error ('Error!');
-            // console.log(res)
             return res.json();
         })
         .then(function (data) {
             console.log(data);
+            return data;
+        })
+}
+
+function fetchCurrentWeather(lat, lon) {
+    var FORECAST_API = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+
+    return fetch(FORECAST_API)
+        .then(function (res) {
+            if (!res.ok) throw new Error ('Error!');
+            return res.json();
+        })
+        .then(function (data) {
+            // console.log(data);
             return data;
         })
 }
@@ -38,7 +50,45 @@ document.getElementById('searchButton').addEventListener('click', function() {
     if (!searchInput.value.trim()) return;
 
     fetchCoordinates(searchInput.value).then(function (coords) {
-        console.log(coords);
-        fetchWeatherForecast(coords[0], coords[1])
+        var lat = coords[0]
+        var lon = coords[1]
+        fetchCurrentWeather(lat, lon).then(function (data) {
+            renderCurrentWeather(data)
+        })
+        fetchWeatherForecast(lat, lon).then(function (data) {
+            renderWeatherForecast(data)
+        })
     });
 })
+
+function renderCurrentWeather(city) {
+    var today = dayjs()
+    var cityDate = document.getElementById('city-date')
+    var currentTemp = document.getElementById('temp')
+    var currentWind = document.getElementById('wind')
+    var currentHumidity = document.getElementById('humidity')
+    cityDate.textContent = city.name + ` (${today.format('DD/MM/YYYY')})`
+    currentTemp.textContent = `Temp: ${city.main.temp}`
+    currentWind.textContent = `Wind: ${city.wind.speed} MPH`
+    currentHumidity.textContent = `Humidity: ${city.main.humidity}%`
+}
+
+function renderWeatherForecast(forecast) {
+    var date = dayjs()
+    var forecastTitle = document.getElementById('forecast-title')
+    var forecastCard = document.getElementById('forecast-cards')
+    var forecastSection = document.getElementById('forecastSection')
+
+    forecastTitle.textContent = '5-Day Forecast:'
+
+    for (var i=0; i<6; i++) {
+        var cardEl = document.createElement('article');
+        var dateEl = document.createElement('p')
+
+        dateEl.textContent = forecast.list[i].dt_txt
+
+        cardEl.appendChild(dateEl)
+        forecastCard.appendChild(cardEl)
+        forecastSection.appendChild(forecastCard)
+    }    
+}
